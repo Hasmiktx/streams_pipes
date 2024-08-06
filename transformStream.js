b87cb6921c3b81ad8d153b48ae7d018f76715a03
Hasmiktx <hasmiktx@gmail.com>
2024-08-06 23:56:49 +0400

    created new branch

--- transformStream.js
+++ transformStream.js
@@ -3,9 +3,25 @@ const {Transform}=require("stream")
 
 const stringTr = new Transform({
     transform(chunk, encoding, callback) {
-        // console.log(chunk)
-      this.push(chunk.toString().toUpperCase());
-      this.push(`timestamp:${new Date()}`);
+        
+        let data;
+    try {
+      data = JSON.parse(chunk);
+      console.log(data,"data")
+    //   for(let line in data){
+    //     console.log(line,"line")
+    //     data[line]=new Date().toString()
+    //   }
+      
+
+      
+    } catch (err) {
+      console.log(err);
+    }
+    
+    //   this.push(chunk.toString().toUpperCase());
+    console.log(data,"data")
+      this.push(JSON.stringify(data));
 
       callback();
     }
